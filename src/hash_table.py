72573e183dcf1203d3d3724b188c4b1660e88b4f
m.possecker <moritzpossecker@gmail.com>
2025-02-24 13:01:29 +0100

    fix: add word values to hash table and show dominat words in buckets

--- src/hash_table.py
+++ src/hash_table.py
@@ -1,8 +1,32 @@
 def create_hash_table(l: int) -> list:
     return list(None for _ in range(l))
 
-def add_to_hash_table(hash_table: list, key: int) -> None:
+
+def add_to_hash_table(hash_table: list, value: str, key: int) -> None:
     if hash_table[key] is None:
         hash_table[key] = []
 
-    hash_table[key].append(key)
+    hash_table[key].append(value)
+
+
+def get_bucket_sizes(hash_table: list) -> list:
+    bucket_sizes = []
+    for bucket in hash_table:
+        if bucket is None:
+            bucket_sizes.append(0)
+        else:
+            bucket_sizes.append(len(bucket))
+    return bucket_sizes
+
+
+def get_dominant_values(hash_table: list) -> list:
+    dominant_values = []
+    for bucket in hash_table:
+        if bucket is None:
+            dominant_values.append(['', 0, 0])
+
+        most_common_word = max(set(bucket), key=bucket.count)
+        count = bucket.count(most_common_word)
+        dominant_values.append([most_common_word, count, int((count / len(bucket)) * 100)])
+
+    return dominant_values
--- src/main.py
+++ src/main.py
@@ -1,27 +1,30 @@
-from hash_table import create_hash_table, add_to_hash_table
+from hash_table import create_hash_table, add_to_hash_table, get_dominant_values, get_bucket_sizes
 from hashing import hash_value
 from text_processor import get_word_list
 from matplotlib import pyplot as plt
 
 
 def plot_table(table: list) -> None:
-    bucket_sizes = []
-    for bucket in hash_table:
-        if bucket is None:
-            bucket_sizes.append(0)
-        else:
-            bucket_sizes.append(len(bucket))
+    dominant_words = get_dominant_values(table)
+    bucket_sizes = get_bucket_sizes(table)
 
     plt.figure(figsize=(10, 5))
-    plt.bar(range(len(bucket_sizes)), bucket_sizes, color='blue', alpha=0.7)
+    bars = plt.bar(range(len(bucket_sizes)), bucket_sizes, color='blue', alpha=0.7)
+
+    for i, bar in enumerate(bars):
+        dominant_word_entry = dominant_words[i]
+        plt.text(bar.get_x() + bar.get_width() / 2, bar.get_height(),
+                 dominant_word_entry[0], ha='center', va='bottom', fontsize=8, rotation=45)
+
     plt.xlabel("Bucket Index")
     plt.ylabel("Number of Elements")
     plt.suptitle("Hash Table Distribution")
-    plt.title("a: " + str(a) + ", b: " + str(b) + ", total words: " + str(len(word_list)))
+    plt.title(f"a: {a}, b: {b}, total words: {len(word_list)}")
     plt.show()
 
 
-m_list = [30, 60, 90]
+
+m_list = [30]
 a = 10
 b = 7
 
@@ -31,6 +34,6 @@ for m in m_list:
     hash_table = create_hash_table(m)
 
     for word in word_list:
-        add_to_hash_table(hash_table, hash_value(word, a, b, m))
+        add_to_hash_table(hash_table, word, hash_value(word, a, b, m))
 
     plot_table(hash_table)
