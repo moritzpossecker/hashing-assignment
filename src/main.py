2b772ebb74c0038f975e1a00a2161ee6d337e27a
m.possecker <moritzpossecker@gmail.com>
2025-02-24 09:11:29 +0100

    feat: hash for multiple m's

--- src/main.py
+++ src/main.py
@@ -3,33 +3,34 @@ from hashing import hash_value
 from text_processor import get_word_list
 from matplotlib import pyplot as plt
 
+
+def plot_table(table: list) -> None:
+    bucket_sizes = []
+    for bucket in hash_table:
+        if bucket is None:
+            bucket_sizes.append(0)
+        else:
+            bucket_sizes.append(len(bucket))
+
+    plt.figure(figsize=(10, 5))
+    plt.bar(range(len(bucket_sizes)), bucket_sizes, color='blue', alpha=0.7)
+    plt.xlabel("Bucket Index")
+    plt.ylabel("Number of Elements")
+    plt.suptitle("Hash Table Distribution")
+    plt.title("a: " + str(a) + ", b: " + str(b) + ", total words: " + str(len(word_list)))
+    plt.show()
+
+
+m_list = [30, 60, 90]
+a = 10
+b = 7
+
 word_list = get_word_list('StringHashing_Schneewittchen.txt')
 
-m = 30
-a = 10
-b = 1307
+for m in m_list:
+    hash_table = create_hash_table(m)
 
-hash_table = create_hash_table(m)
+    for word in word_list:
+        add_to_hash_table(hash_table, hash_value(word, a, b, m))
 
-for word in word_list:
-    add_to_hash_table(hash_table, hash_value(word, a, b, m))
-
-print(word_list)
-print(len(word_list))
-
-print(hash_table)
-
-bucket_sizes = []
-for bucket in hash_table:
-    if bucket is None:
-        bucket_sizes.append(0)
-    else:
-        bucket_sizes.append(len(bucket))
-
-plt.figure(figsize=(10, 5))
-plt.bar(range(len(bucket_sizes)), bucket_sizes, color='blue', alpha=0.7)
-plt.xlabel("Bucket Index")
-plt.ylabel("Number of Elements")
-plt.suptitle("Hash Table Distribution")
-plt.title("a: " + str(a) + ", b: " + str(b) + ", total words: " + str(len(word_list)))
-plt.show()
+    plot_table(hash_table)
